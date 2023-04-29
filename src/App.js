994ecff7322b5545aa001e65fb1a00cefbf9b209
Bellah Ellam <bella.ellam@student.moringaschool.com>
2023-04-29 22:49:59 +0300

    added App. main component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
 
 function App() {
+  const [army, setArmy] = useState([]);
+
+  const addToArmy = (bot) => {
+    if (!army.includes(bot)) {
+      setArmy([...army, bot]);
+    }
+  };
+
+  const removeFromArmy = (bot) => {
+    const updatedArmy = army.filter((b) => b.id !== bot.id);
+    setArmy(updatedArmy);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BotCollection addToArmy={addToArmy} />
+      <YourBotArmy army={army} removeFromArmy={removeFromArmy} />
     </div>
   );
 }
