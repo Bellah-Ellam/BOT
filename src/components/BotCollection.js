48b65c47e5aff700dab5cf7ec6bf65601817b1a3
Bellah Ellam <bella.ellam@student.moringaschool.com>
2023-05-01 23:47:38 +0300

    added sort functionality

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from "react";
 import BotCard from "./BotCard";
+import BotSpecs from "./BotSpecs";
+import SortBar from "./SortBar";
 
 function BotCollection({ addToArmy, army }) {
   const [bots, setBots] = useState([]);
+  const [selectedBot, setSelectedBot] = useState(null);
+  const [filters, setFilters] = useState([]);
+  const [sortKey, setSortKey] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:8001/bots")
@@ -16,19 +21,62 @@ function BotCollection({ addToArmy, army }) {
     }
   };
 
+  const onSort = (key) => {
+    setSortKey(key);
+  };
+
+  const onFilter = (filter) => {
+    setFilters((prevFilters) => {
+      if (prevFilters.includes(filter)) {
+        return prevFilters.filter((f) => f !== filter);
+      } else {
+        return [...prevFilters, filter];
+      }
+    });
+  };
+  if (selectedBot) {
+    return <BotSpecs bot={selectedBot} onEnlist={enlistBot} />;
+  }
+
+  const filteredBots = bots.filter((bot) => {
+    if (filters.length === 0) {
+      return true;
+    } else {
+      return filters.includes(bot.bot_class);
+    }
+  });
+
+  const sortedBots = sortKey
+    ? [...filteredBots].sort((a, b) => b[sortKey] - a[sortKey])
+    : filteredBots;
+
   return (
     <div className="bot-collection">
       <h2>Bots Collection</h2>
+      <SortBar onSort={onSort} />
       <div className="card-container horizontal">
-        {bots.map((bot) => (
-          <BotCard
-            key={bot.id}
-            bot={bot}
-            onEnlist={() => enlistBot(bot)}
+        {selectedBot ? (
+          <BotSpecs
+            bot={selectedBot}
             buttonText="Enlist"
-            disabled={army.find((b) => b.id === bot.id)}
+            onButtonClick={() => {
+              enlistBot(selectedBot);
+              setSelectedBot(null);
+            }}
+            onBackClick={() => setSelectedBot(null)}
           />
-        ))}
+        ) : (
+          sortedBots.map((bot) => (
+            <BotCard
+              key={bot.id}
+              bot={bot}
+              onEnlist={() => enlistBot(bot)}
+              onDetails={() => setSelectedBot(bot)}
+              buttonText="Enlist"
+              disabled={army.find((b) => b.id === bot.id)}
+            />
+          ))
+        )}
       </div>
     </div>
   );
--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -2,32 +2,28 @@ import React from "react";
 import BotCard from "./BotCard";
 
 function YourBotArmy({ army, removeFromArmy, deleteBot }) {
+  const classesEnlisted = army.reduce((classes, bot) => {
+    if (!classes.includes(bot.bot_class)) {
+      classes.push(bot.bot_class);
+    }
+    return classes;
+  }, []);
+
   return (
     <div className="bot-army">
       <h2>Your Bot Army</h2>
       <div className="card-container">
         {army.map((bot) => (
-          // <div className="army-card" key={bot.id}>
-          //   {/* <img src={bot.avatar_url} onClick={() => removeFromArmy(bot)} />
-          //   <p>Health: {bot.health}</p>
-          //   <p>Damage: {bot.damage}</p>
-          //   <p>Armor: {bot.armor}</p>
-          //   <p>Class: {bot.bot_class}</p>
-          //   <p>Catchphrase: {bot.catchphrase}</p>
-          //   <button className="delete-button" onClick={() => deleteBot(bot)}>
-          //     X
-          //   </button> */}
-           
-          // </div>
-           <BotCard className="army-card"
-           bot={bot}
-           onEnlist={removeFromArmy}
-           removeFromArmy={removeFromArmy}
-           deleteBot={deleteBot}
-         />
-
+          <BotCard
+            key={bot.id}
+            bot={bot}
+            onEnlist={removeFromArmy}
+            buttonText="Remove"
+            extraButton={<button onClick={() => deleteBot(bot)}>Delete</button>}
+          />
         ))}
       </div>
+      <p>Classes enlisted: {classesEnlisted.join(", ")}</p>
     </div>
   );
 }
