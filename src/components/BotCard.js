d8f5ef91af98719a89375ad7f9b4b45a9d7bcb65
Bellah Ellam <bella.ellam@student.moringaschool.com>
2023-05-01 22:23:29 +0300

    added remove from backend functionality

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-const BotCard = ({ bot, onEnlist }) => {
+const BotCard = ({ bot, onEnlist, removeFromArmy, deleteBot }) => {
   const { id, name, health, damage, armor, bot_class, catchphrase, avatar_url } = bot;
+
+  function deleteBot(bot) {
+    fetch(`http://localhost:8001/bots/${bot.id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(() => removeFromArmy(bot))
+      .catch((error) => console.error("Error deleting bot", error));
+  }
 
   return (
     <div className="bot-card">
@@ -10,15 +21,21 @@ const BotCard = ({ bot, onEnlist }) => {
         <h3>{name}</h3>
       </div>
       <div className="bot-card-details">
-        <p>Health: {health}</p>
-        <p>Damage: {damage}</p>
-        <p>Armor: {armor}</p>
+        {/* <p><i class="heartbeat icon"></i> {health}</p>
+        <p><i class="icon lightning"></i> {damage}</p>
+        <p><i class="shield alternate icon"></i> {armor}</p> */}
         <p>Class: {bot_class}</p>
         <p>Catchphrase: {catchphrase}</p>
       </div>
-      
-    </div>
+          <button onClick={() => {
+              deleteBot(bot);
+              window.location.reload();
+          }}>Discharge</button>
+          <span><i className="heartbeat icon"></i>{bot.health}</span>
+          <span><i className="icon lightning"></i>{bot.damage}</span>
+          <span><i className="shield alternate icon"></i>{bot.armor}</span>
+      </div>
   );
-}
+};
 
 export default BotCard;
--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,47 +1,31 @@
 import React from "react";
 import BotCard from "./BotCard";
 
-function YourBotArmy({ army, removeFromArmy }) {
-  const releaseBot = (bot) => {
-    removeFromArmy(bot);
-  };
-
-  const deleteBot = (bot) => {
-    fetch(`http://localhost:8001/bots/${bot.id}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    //   body: JSON.stringify(bot),
-    // }).then(() => {
-    //   removeFromArmy(bot);
-    // });
-    }
-    );
-  };
+function YourBotArmy({ army, removeFromArmy, deleteBot }) {
   return (
     <div className="bot-army">
       <h2>Your Bot Army</h2>
       <div className="card-container">
         {army.map((bot) => (
-          <div className="army-card" key={bot.id}>
-          <img src={bot.avatar_url} onClick = {() => releaseBot(bot)}/>
-          <p>Health: {bot.health}</p>
-          <p>Damage: {bot.damage}</p>
-          <p>Armor: {bot.armor}</p>
-          <p>Class: {bot.bot_class}</p>
-          <p>Catchphrase: {bot.catchphrase}</p>
+          // <div className="army-card" key={bot.id}>
+          //   {/* <img src={bot.avatar_url} onClick={() => removeFromArmy(bot)} />
+          //   <p>Health: {bot.health}</p>
+          //   <p>Damage: {bot.damage}</p>
+          //   <p>Armor: {bot.armor}</p>
+          //   <p>Class: {bot.bot_class}</p>
+          //   <p>Catchphrase: {bot.catchphrase}</p>
+          //   <button className="delete-button" onClick={() => deleteBot(bot)}>
+          //     X
+          //   </button> */}
+           
+          // </div>
+           <BotCard className="army-card"
+           bot={bot}
+           onEnlist={removeFromArmy}
+           removeFromArmy={removeFromArmy}
+           deleteBot={deleteBot}
+         />
 
-
-          </div>
-      
-          // <BotCard
-          //   key={bot.id}
-          //   bot={bot}
-          //   onClick={() => releaseBot(bot)}
-          //   buttonText="Release"
-          //   deleteBot={() => deleteBot(bot)}
-          // />
         ))}
       </div>
     </div>
